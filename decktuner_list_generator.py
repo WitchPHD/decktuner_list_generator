353fa6ef7b3d3cec0cf8e645d00c8429566fcbf5
WitchPHD <74690105+WitchPHD@users.noreply.github.com>
2023-05-28 12:47:00 -0400

    Update decktuner_list_generator.py
    Added some tip reporting.
    
    Still to-do: Catch £ and € tips and convert them to $$

--- decktuner_list_generator.py
+++ decktuner_list_generator.py
@@ -158,6 +158,157 @@ def print_workshops():
     #print workshops with users that left
     print("\nWorkshops Whose Pilots Have Left:")
     for x in workshop_list:
+class WORKSHOP:
+    def __init__(self, raw):
+        self.raw = raw
+        self.name = " "
+        self.user_alive = True
+        self.active = True
+        self.claimed = True
+        self.new = False
+        self.urgent = False
+        self.deconstruct(raw)
+    def killuser (self):
+        self.user_alive = False
+    def deactivate (self):
+        self.active = False
+    def flag_new (self):
+        self.new = True
+    def flag_urgent (self):
+        self.urgent = True
+    def deconstruct(self, raw):
+        raw = raw.replace("\"", "\'")
+        try:
+            #get the id
+            tmp_id = re.search("<#.*?>", raw).group()
+            self.id = str(tmp_id[2:-1])
+
+            #get the timestamp
+            tmp_stamp = re.search("'timestamp': '.*?',", raw).group()
+            self.stamp = str(tmp_stamp[14:-2])
+            self.stamp = datetime.datetime.fromisoformat(self.stamp).timestamp()
+
+            #get the budget
+            tmp_budget = re.search("Budget', 'value': '.*?',", raw).group()
+            self.budget =  str(tmp_budget[19:-2]).lower()
+            if self.budget == "no budget":
+                self.budget = "none"
+
+            #get the catagory
+            tmp_cat = re.search("Category', 'value': '.*?',", raw).group()
+            self.cat = str(tmp_cat[21:-2]).lower()
+
+            #get the commander
+            tmp_cmdr = re.search("title': '.*?',", raw).group()
+            self.commander = str(tmp_cmdr[9:-2])
+
+            #get the tip (not in practice yet)
+            try:
+                tmp_tip = re.search("Tip Amount', 'value': '.*?',", raw).group()
+                self.tip = str(tmp_tip[23:-2])
+                #convert the tip to a number
+                self.tip = int(re.sub('[^0-9]','', self.tip))
+            except:
+                self.tip = 0
+
+            #get the pilot
+            tmp_pilot = re.search("Pilot', 'value': '<@.*?>',", raw).group()
+            self.pilot = str(tmp_pilot[20:-3])
+      
+            print("\nNEW WORKSHOP:", raw)
+        except:
+            print("\n Error in workshop:", raw)
+
+def retrieve_messages(channel_ID, amount):
+    headers={
+        "authorization" : auth
+    }
+    r = requests.get("https://discord.com/api/v9/channels/{:}/messages?limit={:}".format(channel_ID, amount), headers=headers)
+    message_raw = json.loads(r.text)
+    return message_raw
+
+def retrieve_channels(server_ID):
+    headers={
+        "authorization" : auth
+    }
+    r = requests.get("https://discord.com/api/v9/guilds/{:}/channels".format(server_ID), headers=headers)
+    channel_raw = json.loads(r.text)
+    #time info put outside of loop to increase scope
+    check_time_stamp = datetime.datetime.now().timestamp()
+    days = 86400
+    for x in channel_raw:
+        #check tuning boards for inactive workshops first
+        if "tuning-board" in x["name"]:
+            for y in retrieve_messages(x["id"], 100):
+                #deconstruct raw message data from turning board into a workshop element and append it to the workshop list
+                workshop_list.append(WORKSHOP(str(y["embeds"])))
+                #get recent messages from tuning boards
+                info = str(y["embeds"])
+                tmp_id = re.search("<#.*?>", info).group()
+                if "'name': 'Tuners', 'value': '*none*'" in str(y["embeds"]):
+                    unclaimed_ids.append(str(tmp_id[2:-1])) #remove the <# and >
+                    print("{:} appended to unclaimed IDs".format(tmp_id[2:-1]))
+        #check spam logs for users that left
+        if "spam-logs" in x["name"]:
+            spam_messages = retrieve_messages(x["id"], 100)
+            for y in spam_messages:
+                spam_raw = str(y["embeds"])
+                if "Member left" in spam_raw:
+                    tmp_left_id = re.search("<@.*?>", spam_raw).group()
+                    real_left_id = str(tmp_left_id[2:-1])
+                    user_left_ids.append(real_left_id)
+                    print("User {:} has left the server.".format(real_left_id))
+    for x in channel_raw:
+        #check workshop lists
+        if "workshop" in x["name"]:
+            #find workshop in list and set the name
+            for y in workshop_list:
+                if y.id == x["id"]:
+                    y.name = x["name"]
+            try:
+                #find the workshop in the workshop list
+                for y in workshop_list:
+                    if y.id == x["id"]:
+                        if y.pilot in user_left_ids:
+                            #if pilot left, kill user
+                            y.killuser()
+                            print("Workshop {:} killed".format(x["id"]))
+                        if y.id in unclaimed_ids:
+                            #if id unclaimed, set unclaimed
+                            y.claimed = False
+                            print("Append {:} to unclaimed - ID match: {:}".format(x["name"], x["id"]))#check timestamp
+                recent_activity = retrieve_messages(x["id"], 1)
+                #check the messages timestamp and compare to now()
+                for y in recent_activity:
+                    message_time = datetime.datetime.fromisoformat(y["timestamp"]).timestamp()
+                    if check_time_stamp - message_time > 20*days:
+                        for z in workshop_list:
+                            if z.id == x["id"]:
+                                z.deactivate()
+                #check the workshop creation 
+                for z in workshop_list:
+                    if check_time_stamp - z.stamp < 7*days:
+                        z.flag_new()
+                    if check_time_stamp - z.stamp > 20*days:
+                        z.flag_urgent()
+            except:
+                print("Error for {}.".format(x["name"]))
+
+def print_workshops():
+    #create some counters for math and printing
+    workshop_list.reverse()
+    inactive_claimed = 0
+    unclaimed = 0
+    inactive = 0
+    high_tip = 0
+    high_name = "name"
+    tip_tot = 0
+    urgent = 0
+    new = 0
+
+    #print workshops with users that left
+    print("\nWorkshops Whose Pilots Have Left:")
+    for x in workshop_list:
         if x.user_alive == False:
             print(" - #{:}".format(x.name))
 
@@ -183,6 +334,9 @@ def print_workshops():
                 entry += " | :dollar:**TIP: {:} $$** ".format(x.tip)
                 #add tip to tip total
                 tip_tot += x.tip
+                if x.tip > high_tip:
+                    high_tip = x.tip
+                    high_name = x.name
             if x.new == True:
                 entry += " _(new)_"
                 new += 1
@@ -195,6 +349,10 @@ def print_workshops():
             if line_counter == 20:
                 print("\n")
                 line_counter = 0
+    #print bounty report
+    print("\n:dollar:**Workshop Bounty**:dollar: ")
+    print(" - {:} $$ total is available in unclaimed tips.".format(tip_tot))
+    print(" - Highest bounty amount: {:} $$ in #{:}".format(high_tip, high_name))
 
     #find active workshop count and latest unclaimed shop number for maths
     tot = len(workshop_list)
@@ -208,9 +366,8 @@ def print_workshops():
     print(" - {:} ({:.2f}%) total workshops have have been inactive for more than 20 days.".format(inactive, 100*(inactive/tot)))
     print(" - {:} ({:.2f}%) of inactive workshops that have been claimed by a tuner already.".format(inactive_claimed, 100*(inactive_claimed/inactive)))
     print(" - {:} total workshop requests received; {:} ({:.2f}%) of all-time workshops have been closed.".format(alltime_tot, alltime_tot - tot, 100*((alltime_tot - tot)/alltime_tot) ))
-    print(" - {:} USD is available in unclaimed tips.".format(tip_tot))
+
 
 if __name__ == "__main__":
     retrieve_channels(decktuner)
     print_workshops()
-    
